b303064b9abe5e91181fe74251703a907f5d1444
Sajith Abeygunawardana <abeygunawardana1995@gmail.com>
2023-06-29 21:28:02 +0530

    finalize the node-js-back-end & close #3

--- backend-nodejs/src/main.ts
+++ backend-nodejs/src/main.ts
@@ -1,8 +1,9 @@
 import express, {json} from 'express'
+import {router as CourseHttpController} from "./api/courseHttpCotroller";
 import cors from 'cors';
 const app = express();
 
 app.use(cors());
 app.use(json());
-app.use('/api/v1/books',)
+app.use('/api/v1/courses',CourseHttpController)
 app.listen(8080, ()=> console.log("Server has been started at 8080"));
