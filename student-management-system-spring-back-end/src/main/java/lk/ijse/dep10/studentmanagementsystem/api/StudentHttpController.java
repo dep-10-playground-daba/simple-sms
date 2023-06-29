19fa6c8d765796c3a7e5dd88a908d6962cae5851
nishan akalanka <hdnakalanka@gmail.com>
2023-06-29 20:16:49 +0530

    xposing Save-Get-Delete Web Services

--- student-management-system-spring-back-end/src/main/java/lk/ijse/dep10/studentmanagementsystem/api/StudentHttpController.java
+++ student-management-system-spring-back-end/src/main/java/lk/ijse/dep10/studentmanagementsystem/api/StudentHttpController.java
@@ -1,4 +1,23 @@
 package lk.ijse.dep10.studentmanagementsystem.api;
 
+import lk.ijse.dep10.studentmanagementsystem.entity.Student;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/students")
 public class StudentHttpController {
+    @GetMapping
+    public String getAllStudents(){
+        return "<h1>Get request Working<h1>";
+    }
+    @PostMapping
+    public String SaveStudent(){
+        return "<h1>Save Student Working<h1>";
+    }
+    @DeleteMapping("/{studentId}")
+    public String DeleteStudent(){
+        return "<h1>Delete Student Working<h1>";
+    }
+
 }
---
+++ student-management-system-spring-back-end/src/main/java/lk/ijse/dep10/studentmanagementsystem/entity/Student.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep10.studentmanagementsystem.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    private String id;
+    private String name;
+    private String address;
+}
